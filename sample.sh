a97623392952bc8dbfc9618beef387bd3588d3ac
MandarAcharekar <mandarmva8055@gmail.com>
2018-03-12 09:09:45 +0530

    Login Verification Works!

--- sample.sh
+++ sample.sh
@@ -5,7 +5,7 @@ echo "Enter your choice:"
 read ch
 case $ch in
 
-1)echo "Creating new Account...."
+1)echo -e "\t\tCreating new Account...."
 echo "Enter Username:"
 read user
 echo "Enter password:"
@@ -25,7 +25,7 @@ EOF
 ;;
 
 
-2)echo "Loging into Existinfg Account...."
+2)echo -e "\t\tLoging into Existinfg Account...."
 echo "Enter Username":
 read user
 echo "Enter Password:"
@@ -33,29 +33,24 @@ read -s pass
 
 hash_check=`echo $pass | sha256sum`
 
-:'
-mysql -u root -pmandar<<EOF
-use abcd;
-select * from Storage where password = "${hash_check}"
-AND username = "$user";
 
-select username from Storage where password = "${hash_check}"
-AND username = "$user";
-select password from Storage where password = "${hash_check}"
-AND username = "$user";
-EOF
-'
+#echo -e "\nNOW BASH COMMANDS:"
+my_user=$(echo "select username from Storage where password =\"${hash_check}\" AND username = \"$user\" ;" | mysql abcd -u root -pmandar -N )
+my_pass=$(echo "select password from Storage where password=\"${hash_check}\" AND username=\"$user\";" | mysql abcd -u root -pmandar -N)
 
-echo -e "NOW BASH COMMANDS:\n"
-my_user=$(echo "select username from Storage where password =\"${hash_check}\" AND username = \"$user\" ;" | mysql abcd -u root -pmandar )
-my_pass=$(echo "select password from Storage where password=\"${hash_check}\" AND username=\"$user\";" | mysql abcd -u root -pmandar)
+echo -e "USERNAME: $my_user\nPASSWORD: $my_pass\n"
 
-echo -e "USERNAME: $my_user\n PASSWORD: $my_pass\n"
+user_len=$(expr length "$my_user") #Bcoz username takes 8 char. and 1 new line char.
 
+user_pass=$(expr length "$my_pass") #bcoz password takes 9 char. and 1 new line char.
 
+#echo "$user_len"
+#echo "$user_pass"
 
-
-
+#user_actual=$(echo $str | cut -d' ' -f 2)
+if [ $user_len -gt 0 ]; then 
+	echo -e "\t\tWELCOME $my_user !\n\t\tLOGIN SUCCESFULL!\n"
+fi
 
 ;;
 esac
